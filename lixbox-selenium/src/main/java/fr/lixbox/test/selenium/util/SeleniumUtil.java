c3eed9ad85b994422576c2195fbad90aaad13fc7
TERRAL Ludovic <ludovic.terral@dev.lan>
2020-07-06 10:58:39 +0200

    maj entete opensource

--- lixbox-selenium/src/main/java/fr/lixbox/test/selenium/util/SeleniumUtil.java
+++ lixbox-selenium/src/main/java/fr/lixbox/test/selenium/util/SeleniumUtil.java
@@ -3,14 +3,22 @@
  *                           FRAMEWORK Lixbox
  *                          ==================
  *      
- *   Copyrigth - LIXTEC - Tous droits reserves.
+ * This file is part of lixbox-test.
+ *
+ *    lixbox-supervision is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    lixbox-supervision is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *    along with lixbox-test.  If not, see <https://www.gnu.org/licenses/>
  *   
- *   Le contenu de ce fichier est la propriete de la societe Lixtec.
- *   
- *   Toute utilisation de ce fichier et des informations, sous n'importe quelle
- *   forme necessite un accord ecrit explicite des auteurs
- *   
- *   @AUTHOR Ludovic TERRAL
+ *   @AUTHOR Lixbox-team
  *
  ******************************************************************************/
 package fr.lixbox.test.selenium.util;
