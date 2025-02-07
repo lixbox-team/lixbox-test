46176cef3cf1a2da53bec6d574248d7f78f2cdf3
Ludovic TERRAL <ludovic.terral@lixtec.fr>
2025-02-07 14:13:02 +0100

    initial version jdk-17

--- gradle/config/environment.groovy
+++ gradle/config/environment.groovy
@@ -1,9 +1,9 @@
 desc_project{
     type="library"
     version{
-        majorVersion=11
-        mediumVersion=1
-        minorVersion=2
+        majorVersion=17
+        mediumVersion=0
+        minorVersion=0
     }
     artefact{
         group="fr.lixbox.lixbox-test"
